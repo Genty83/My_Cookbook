0eabe92e7b866bd25744f621d5ef49d32ab4b02c
Craig Gent <genty83@outlook.com>
2024-06-16 12:39:13 +0000

    Added new functions to routes file

--- src/routes.py
+++ src/routes.py
@@ -11,3 +11,27 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+# View Recipes 
+@app.route("/view_recipes")
+def view_recipes():
+    return render_template("view_recipes.html")
+
+
+# Create Recipe
+@app.route("/create_recipe")
+def create_recipe():
+    return render_template("create_recipe.html")
+
+
+# About
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# Accounts
+@app.route("/accounts")
+def accounts():
+    return render_template("accounts.html")
