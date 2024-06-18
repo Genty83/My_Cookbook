deacf149f9e33d4b7fe3250cb39d533d29e1f009
Craig Gent <genty83@outlook.com>
2024-06-18 13:30:25 +0000

    Udated file structure

--- src/routes.py
+++ src/routes.py
@@ -10,28 +10,28 @@ from flask import render_template
 # Homepage
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("pages/home.html")
 
 
 # View Recipes 
 @app.route("/view_recipes")
 def view_recipes():
-    return render_template("view_recipes.html")
+    return render_template("pages/view_recipes.html")
 
 
 # Create Recipe
 @app.route("/create_recipe")
 def create_recipe():
-    return render_template("create_recipe.html")
+    return render_template("pages/create_recipe.html")
 
 
 # About
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("pages/about.html")
 
 
 # Accounts
 @app.route("/accounts", methods=["GET", "POST"])
 def accounts():
-    return render_template("accounts.html")
+    return render_template("pages/accounts.html")
